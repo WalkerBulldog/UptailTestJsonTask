6b9fe4686b570bbc92f9e389e66fccd5a7c417fe
snezhkalesha@gmail.com <snezhkalesha@gmail.com>
2022-09-28 00:44:49 +0300

    Добавьте файлы проекта.

---
+++ UptailTestJsonTask/JsonValueFinder.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UptailTestJsonTask
+{
+    /// <summary>
+    /// Class for search any json element value.
+    /// </summary>
+    public static class JsonValueFinder
+    {
+        /// <summary>
+        /// Gets value of jsonElement.
+        /// </summary>
+        /// <param name="filePath">Path to json file.</param>
+        /// <param name="jsonValuePath">Path to json element.</param>
+        /// <returns>Json element value.</returns>
+        /// <exception cref="NullReferenceException">File was empty.</exception>
+        public static string GetValue(string filePath, string jsonValuePath)
+        {
+            string json = File.ReadAllText(filePath);
+            if (string.IsNullOrEmpty(json))
+            {
+                throw new NullReferenceException(string.Format("{0} file is empty!", jsonValuePath));
+            }
+            var jsonObject = JToken.Parse(json);
+            var token = jsonObject.SelectToken(jsonValuePath);
+            if (token is null)
+            {
+                throw new NullReferenceException(string.Format("Didn't find any element with {0} path!", jsonValuePath));
+            }
+            return token.ToString();
+        }
+    }
+}
---
+++ UptailTestJsonTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using UptailTestJsonTask;
+
+string filePath = "file.json";
+string jsonValuePath = "Indo-European.Slavic.East-Slavic.[0]";
+try
+{
+    Console.WriteLine(string.Format("Value of {0} is {1}",
+    jsonValuePath, JsonValueFinder.GetValue(filePath, jsonValuePath)));
+}
+catch(Exception e)
+{ 
+    Console.WriteLine(e.ToString()); 
+}
+
+
